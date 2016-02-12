201f986b43931c175891d084a8066fc806f2959b
amandakifer <amandakifer@gmail.com>
2016-02-11 16:52:47 -0800

    Created starter django app.

--- weather/urls.py
+++ weather/urls.py
@@ -19,3 +19,10 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
+]
